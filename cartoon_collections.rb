5ffb95e6697762f0c500e626fa613604eb2474d7
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-15 03:48:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(list)# code an argument here
   # Your code here
   new_list = []
 
-  list.each_with_index(1) do |dwarf, index|
+  list.each.with_index(1) do |dwarf, index|
     puts "#{index}. #{dwarf}"
   end
 end
